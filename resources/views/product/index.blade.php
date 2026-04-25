b616c654cfd8c7ad2fbb20397e58e9a1b19ed6f2
aivylv <lyvialarasati@gmail.com>
2026-04-25 21:37:45 +0700

    Pertemuan 7: Component Berjalan

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -12,15 +12,7 @@
                         </div>
                         
                         @can('manage-product')
-                        <a href="{{ route('product.create') }}"
-                        class="inline-flex items-center gap-2 px-4 py-2.5 rounded-lg bg-gray-200 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 text-gray-800 dark:text-white text-sm font-semibold transition">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
-                            </svg>
-
-                            Add Product
-                        </a>
+                            <x-add-product :url="route('product.create')" name="Add Product" />
                         @endcan
 
                         @can('export-product')
